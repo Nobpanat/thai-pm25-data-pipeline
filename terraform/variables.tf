d40822a947f4f66fcf9d39f252337cba9bf37acb
nobpanat <noppana08@gmail.com>
2026-04-21 04:33:00 +0700

    add variable terraform.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,34 +1,35 @@
 variable "credentials" {
   description = "Path to GCP Service Account Key"
+  type        = string
   default     = "./keys/credentials.json"
 }
 
 variable "project" {
   description = "GCP Project ID"
-  default     = "thai25pm-data-platform"
+  type        = string
 }
 
 variable "region" {
   description = "GCP Region"
-  default     = "asia-southeast1"
+  type        = string
 }
 
 variable "location" {
   description = "Project Location for BQ and GCS"
-  default     = "ASIA-SOUTHEAST1"
+  type        = string
 }
 
 variable "gcs_bucket_name" {
   description = "My GCS Bucket Name"
-  default     = "thai25pm-data-lake-bucket-252026"
+  type        = string
 }
 
 variable "bq_dataset" {
   description = "BigQuery Dataset for Raw/Staging Data"
-  default     = "thai25pm_dataset_252026"
+  type        = string
 }
 
 variable "bq_dataset_prod" {
   description = "BigQuery Dataset for Production Data"
-  default     = "thai25pm_data_prod_252026"
+  type        = string
 }
